6da85e6c3cb7196a4eb706882cb1c868a6566d79
Peter Vrenken <peter.vrenken@atos.net>
2022-09-08 20:47:33 +0200

    Moved file.

---
+++ Source/Prototype2/EtAlii.Them.Game.Unity/Assets/Game/Players/PlayerBehavior.cs
@@ -0,0 +1,35 @@
+namespace Game.Units
+{
+    using System.Collections;
+    // using Game.Buildings;
+    using UnityEngine;
+
+    public class PlayerBehavior : MonoBehaviour
+    {
+        public IEnumerable MoveTo(Vector2 position)
+        {
+            yield return null;
+        }
+        // public IEnumerable CreateBuilding<TBuilding>(Vector2 position)
+        //     where TBuilding: Building
+        // {
+        //     yield return null;
+        // }
+        // public IEnumerable Configure<TBuilding>(Vector2 position)
+        //     where TBuilding: Building
+        // {
+        //     yield return null;
+        // }
+        //
+        // public IEnumerable CreateUnit<TUnit>(Vector2 position)
+        //     where TUnit: Unit
+        // {
+        //     yield return null;
+        // }
+        // public IEnumerable RepairUnit<TUnit>(Vector2 position)
+        //     where TUnit: Unit
+        // {
+        //     yield return null;
+        // }
+    }
+}
