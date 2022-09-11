1b483bbef07fc28cfcefd892632015c4b6d4af8c
Peter Vrenken <peter.vrenken@atos.net>
2022-09-11 18:17:14 +0200

    Removed Rider warnings.

--- Source/Prototype2/EtAlii.Them.Game.Unity/Assets/Game/Buildings/BuildingFactory.cs
+++ Source/Prototype2/EtAlii.Them.Game.Unity/Assets/Game/Buildings/BuildingFactory.cs
@@ -1,14 +1,14 @@
 namespace Game.Buildings
 {
-    using System.Collections;
-    using UnityEngine;
-    using Game.TechTree;
+    // using System.Collections;
+    // using UnityEngine;
+    // using Game.TechTree;
 
     public class BuildingFactory
     {
-        IEnumerable Instantiate(Vector2 position, BuildingTemplate template)
-        {
-            yield return null;
-        }
+        // IEnumerable Instantiate(Vector2 position, BuildingTemplate template)
+        // {
+        //     yield return null;
+        // }
     }
 }
