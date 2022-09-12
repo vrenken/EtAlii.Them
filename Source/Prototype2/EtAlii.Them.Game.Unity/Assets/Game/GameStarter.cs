24a808be5c87d0fcb59127662340336a65ad8f56
Peter Vrenken <peter.vrenken@atos.net>
2022-09-12 23:32:24 +0200

    Added GameStarter in an attempt to get rid of a weird Unity compilation warning.

---
+++ Source/Prototype2/EtAlii.Them.Game.Unity/Assets/Game/GameStarter.cs
@@ -0,0 +1,13 @@
+namespace Game
+{
+    using UnityEngine;
+
+    public static class GameStarter
+    {
+        [RuntimeInitializeOnLoadMethod]
+        public static void Start()
+        {
+            
+        }
+    }
+}
